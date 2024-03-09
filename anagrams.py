0aa0f218ed4f506fe8cf601ec9055bbc1e545b6f
Connor <connordbrown@gmail.com>
2024-03-08 19:19:21 -0700

    add tests for anagrams.py

--- anagrams.py
+++ anagrams.py
@@ -6,3 +6,15 @@ def char_count(s):
   for char in s:
     count[char] = count.get(char, 0) + 1
   return count
+
+assert anagrams('restful', 'fluster') == True
+assert anagrams('cats', 'tocs') == False
+assert anagrams('monkeyswrite', 'newyorktimes') == True
+assert anagrams('paper', 'reapa') == False
+assert anagrams('elbow', 'below') == True
+assert anagrams('tax', 'taxi') == False
+assert anagrams('taxi', 'tax') == False
+assert anagrams('night', 'thing') == True
+assert anagrams('abbc', 'aabc') == False
+assert anagrams('po', 'popp') == False
+assert anagrams('pp', 'oo') == False
