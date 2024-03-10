42e500973be9c92fa03afdae8a237dce4df808de
Connor <connordbrown@gmail.com>
2024-03-09 20:06:12 -0700

    add tests for most_frequent_character.py

--- most_frequent_char.py
+++ most_frequent_char.py
@@ -8,3 +8,11 @@ def most_frequent_char(s):
     if most_frequent is None or count_dict[char] > count_dict[most_frequent]:
       most_frequent = char
   return most_frequent
+
+assert most_frequent_char('bookkeeper') == 'e'
+assert most_frequent_char('david') ==  'd'
+assert most_frequent_char('abby') == 'b' 
+assert most_frequent_char('mississippi') == 'i' 
+assert most_frequent_char('potato') == 'o' 
+assert most_frequent_char('eleventennine') == 'e'
+assert most_frequent_char('riverbed') ==  'r'
