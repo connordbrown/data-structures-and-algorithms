f9b3950d720eb65292edc334255e4fd6b5fc7d62
Connor <connordbrown@gmail.com>
2024-03-09 20:07:25 -0700

    add solution for pair_sum.py

---
+++ pair_sum.py
@@ -0,0 +1,10 @@
+def pair_sum(numbers, target_sum):
+  previous_numbers = {}
+  
+  for index, num in enumerate(numbers):
+    complement = target_sum - num
+    
+    if complement in previous_numbers:
+      return (index, previous_numbers[complement])
+    
+    previous_numbers[num] = index
