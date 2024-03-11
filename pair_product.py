e08b8a54568562cebe6f40504e6665a98a631916
Connor <connordbrown@gmail.com>
2024-03-10 20:30:24 -0600

    add tests for pair_product.py

--- pair_product.py
+++ pair_product.py
@@ -9,3 +9,10 @@ def pair_product(numbers, target_product):
   
     # put num into dictionary if not present
     previous_numbers[num] = index
+
+pair_product([3, 2, 5, 4, 1], 8) # -> (1, 3)
+pair_product([3, 2, 5, 4, 1], 10) # -> (1, 2)
+pair_product([4, 7, 9, 2, 5, 1], 5) # -> (4, 5)
+pair_product([4, 7, 9, 2, 5, 1], 35) # -> (1, 4)
+pair_product([3, 2, 5, 4, 1], 10) # -> (1, 2)
+pair_product([4, 6, 8, 2], 16) # -> (2, 3)
