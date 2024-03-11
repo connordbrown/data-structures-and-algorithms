4c8b65630d5ed65b20815a9ebbc8f8b4d37d7083
Connor <connordbrown@gmail.com>
2024-03-10 20:31:51 -0600

    update tests, fix typo

--- pair_product.py
+++ pair_product.py
@@ -5,14 +5,14 @@ def pair_product(numbers, target_product):
     complement = target_product / num
     
     if complement in previous_numbers:
-      return (index, previous_numbers[complement]) # current index, complement index
+      return (previous_numbers[complement], index) # current index, complement index
   
     # put num into dictionary if not present
     previous_numbers[num] = index
 
-pair_product([3, 2, 5, 4, 1], 8) # -> (1, 3)
-pair_product([3, 2, 5, 4, 1], 10) # -> (1, 2)
-pair_product([4, 7, 9, 2, 5, 1], 5) # -> (4, 5)
-pair_product([4, 7, 9, 2, 5, 1], 35) # -> (1, 4)
-pair_product([3, 2, 5, 4, 1], 10) # -> (1, 2)
-pair_product([4, 6, 8, 2], 16) # -> (2, 3)
+assert pair_product([3, 2, 5, 4, 1], 8) == (1, 3)
+assert pair_product([3, 2, 5, 4, 1], 10) == (1, 2)
+assert pair_product([4, 7, 9, 2, 5, 1], 5) == (4, 5)
+assert pair_product([4, 7, 9, 2, 5, 1], 35) == (1, 4)
+assert pair_product([3, 2, 5, 4, 1], 10) == (1, 2)
+assert pair_product([4, 6, 8, 2], 16) == (2, 3)
