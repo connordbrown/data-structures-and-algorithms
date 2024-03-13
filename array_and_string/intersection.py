8d94730b9080946880544b25405ba2abafc3ee8e
Connor <connordbrown@gmail.com>
2024-03-12 21:02:24 -0600

    add tests for intersection.py

--- array_and_string/intersection.py
+++ array_and_string/intersection.py
@@ -1,3 +1,12 @@
 def intersection(a, b):
   item_set = set(a)
   return [item for item in b if item in item_set]
+
+assert intersection([4,2,1,6], [3,6,9,2,10]) == [6, 2]
+assert intersection([2,4,6], [4,2]) == [4, 2]
+assert intersection([4,2,1], [1,2,4,6]) == [1, 2, 4]
+assert intersection([0,1,2], [10,11]) == []
+
+a = [ i for i in range(0, 50000) ]
+b = [ i for i in range(0, 50000) ]
+assert intersection(a, b) == list(range(50000))
