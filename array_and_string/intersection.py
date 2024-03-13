d2b2487249f52bd50c8e26fe3ab41027b35ef711
Connor <connordbrown@gmail.com>
2024-03-12 21:08:49 -0600

    include robust solution

--- array_and_string/intersection.py
+++ array_and_string/intersection.py
@@ -1,6 +1,11 @@
+# assumes no duplicates
 def intersection(a, b):
   item_set = set(a)
   return [item for item in b if item in item_set]
+
+# # more robust - accounts for duplicates
+# def intersection(a, b):
+#   return list(set(a).intersection(set(b)))
 
 assert intersection([4,2,1,6], [3,6,9,2,10]) == [6, 2]
 assert intersection([2,4,6], [4,2]) == [4, 2]
