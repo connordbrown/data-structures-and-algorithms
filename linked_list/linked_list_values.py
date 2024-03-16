a43013bb6a3364532a0283699d5ad6cace3afb2f
Connor <connordbrown@gmail.com>
2024-03-15 19:30:41 -0600

    add recursive solution for linked_list_values.py

--- linked_list/linked_list_values.py
+++ linked_list/linked_list_values.py
@@ -10,3 +10,16 @@ def linked_list_values(head):
         values.append(current)
         current = current.next
     return values
+
+# helper function allows pass by reference of values list, preventing
+# copies from being creating and slowing down runtime
+def fill_values(head, values):
+    if head is None:
+        return
+    values.append(head.val)
+    fill_values(head.next, values)
+
+def linked_list_values(head):
+    values = []
+    fill_values(head, values)
+    return values
