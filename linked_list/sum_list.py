74c7a86a268d74bb2c75193a6a0ebc8760139522
Connor <connordbrown@gmail.com>
2024-03-17 20:03:21 -0600

    add tests for sum_list

--- linked_list/sum_list.py
+++ linked_list/sum_list.py
@@ -10,3 +10,39 @@ def sum_list(head):
     sum += current.val
     current = current.next
   return sum
+
+# test_01
+a = Node(2)
+b = Node(8)
+c = Node(3)
+d = Node(-1)
+e = Node(7)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+# 2 -> 8 -> 3 -> -1 -> 7
+
+assert sum_list(a) == 19
+
+# test_02
+x = Node(38)
+y = Node(4)
+
+x.next = y
+
+# 38 -> 4
+
+assert sum_list(x) == 42
+
+# test_03
+z = Node(100)
+
+# 100
+
+assert sum_list(z) == 100
+
+# test_04
+assert sum_list(None) == 0
