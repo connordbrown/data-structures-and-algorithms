e6967683e82206206fad2ecc623c8834953aee47
Connor <connordbrown@gmail.com>
2024-03-17 20:00:58 -0600

    add iterative solution for sum_list

---
+++ linked_list/sum_list.py
@@ -0,0 +1,12 @@
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+def sum_list(head):
+  sum = 0
+  current = head
+  while current is not None:
+    sum += current.val
+    current = current.next
+  return sum
