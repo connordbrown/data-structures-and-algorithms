b458834d629075875460197441fa95b6a1ccc7be
Connor <connordbrown@gmail.com>
2024-03-18 20:04:42 -0600

    add tests for linked_list_find

--- linked_list/linked_list_find.py
+++ linked_list/linked_list_find.py
@@ -10,3 +10,61 @@ def linked_list_find(head, target):
       return True
     current = current.next
   return False
+
+# test_01
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+
+linked_list_find(a, "c") # True
+
+# test_02
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+linked_list_find(a, "d") # True
+
+# test_03
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+linked_list_find(a, "q") # False
+
+# test_04
+node1 = Node("jason")
+node2 = Node("leneli")
+
+node1.next = node2
+# jason -> leneli
+linked_list_find(node1, "jason") # True
+
+# test_05
+node1 = Node(42)
+# 42
+linked_list_find(node1, 42) # True
+
+# test_06
+node1 = Node(42)
+# 42
+linked_list_find(node1, 100) # False
