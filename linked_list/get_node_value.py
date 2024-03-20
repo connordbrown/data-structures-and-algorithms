d322fc4701f5769b6399ae37e1010e95bd8c19e9
Connor <connordbrown@gmail.com>
2024-03-19 20:40:00 -0600

    add tests for get_node_value

--- linked_list/get_node_value.py
+++ linked_list/get_node_value.py
@@ -12,3 +12,57 @@ def get_node_value(head, index):
     count += 1
     current = current.next
   return None
+
+# test_01
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+assert get_node_value(a, 2) == 'c'
+
+# test_02
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+assert get_node_value(a, 3) == 'd'
+
+# test_03
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+assert get_node_value(a, 7) == None
+
+# test_04
+node1 = Node("banana")
+node2 = Node("mango")
+node1.next = node2
+
+# banana -> mango
+assert get_node_value(node1, 0) == 'banana'
+
+# test_05
+node1 = Node("banana")
+node2 = Node("mango")
+node1.next = node2
+
+# banana -> mango
+assert get_node_value(node1, 1) == 'mango'
