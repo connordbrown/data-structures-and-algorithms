3d7a764f3c09236408d2178fa424d9f5b4c3e7bc
Connor <connordbrown@gmail.com>
2024-03-20 21:11:45 -0600

    add tests for reverse_list

--- linked_list/reverse_list.py
+++ linked_list/reverse_list.py
@@ -12,3 +12,33 @@ def reverse_list(head):
     prev = current
     current = next
   return prev
+
+# test_01
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+# a -> b -> c -> d -> e -> f
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+# f -> e -> d -> c -> b -> a
+reverse_list(a) 
+
+# test_02
+x = Node("x")
+y = Node("y")
+
+# x -> y
+x.next = y
+# y -> x
+reverse_list(x)
+
+# test_03
+p = Node("p")
+# p
+reverse_list(p) # p
