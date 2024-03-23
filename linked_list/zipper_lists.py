b2a99bbad9f1348417e0cdbe65eada82f6e5c79f
Connor <connordbrown@gmail.com>
2024-03-22 19:45:41 -0600

    add tests for zipper_lists

--- linked_list/zipper_lists.py
+++ linked_list/zipper_lists.py
@@ -25,3 +25,92 @@ def zipper_lists(head_1, head_2):
     tail.next = current_2
     
   return head_1
+
+# test_01
+a = Node("a")
+b = Node("b")
+c = Node("c")
+a.next = b
+b.next = c
+# a -> b -> c
+
+x = Node("x")
+y = Node("y")
+z = Node("z")
+x.next = y
+y.next = z
+# x -> y -> z
+
+# a -> x -> b -> y -> c -> z
+zipper_lists(a, x)
+
+
+# test_02
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+# a -> b -> c -> d -> e -> f
+
+x = Node("x")
+y = Node("y")
+z = Node("z")
+x.next = y
+y.next = z
+# x -> y -> z
+
+# a -> x -> b -> y -> c -> z -> d -> e -> f
+zipper_lists(a, x)
+
+# test_03
+s = Node("s")
+t = Node("t")
+s.next = t
+# s -> t
+
+one = Node(1)
+two = Node(2)
+three = Node(3)
+four = Node(4)
+one.next = two
+two.next = three
+three.next = four
+# 1 -> 2 -> 3 -> 4
+
+# s -> 1 -> t -> 2 -> 3 -> 4
+zipper_lists(s, one)
+
+# test_04
+w = Node("w")
+# w
+
+one = Node(1)
+two = Node(2)
+three = Node(3)
+one.next = two
+two.next = three
+# 1 -> 2 -> 3 
+
+# w -> 1 -> 2 -> 3
+zipper_lists(w, one)
+
+# test_05
+one = Node(1)
+two = Node(2)
+three = Node(3)
+one.next = two
+two.next = three
+# 1 -> 2 -> 3 
+
+w = Node("w")
+# w
+
+# 1 -> w -> 2 -> 3
+zipper_lists(one, w)
