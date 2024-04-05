0e6b9e8d535af6a173621d777f755acb1eb54269
Connor <connordbrown@gmail.com>
2024-04-04 19:24:05 -0600

    add recursive solution for is_univalue_list

--- linked_list/is_univalue_list.py
+++ linked_list/is_univalue_list.py
@@ -11,6 +11,13 @@ def is_univalue_list(head):
     current = current.next
   return True
 
+# def is_univalue_list(head, prev_val = None):
+#   if head is None:
+#     return True
+#   if prev_val is not None and head.val != prev_val:
+#     return False
+#   return is_univalue_list(head.next, head.val)
+
 # test_01
 a = Node(7)
 b = Node(7)
