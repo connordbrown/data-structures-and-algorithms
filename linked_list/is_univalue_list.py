3ecac4121ea8cf14ba68929ba6174989f94defe5
Connor <connordbrown@gmail.com>
2024-04-04 19:22:36 -0600

    add tests for is_univalue_list

--- linked_list/is_univalue_list.py
+++ linked_list/is_univalue_list.py
@@ -10,3 +10,82 @@ def is_univalue_list(head):
       return False
     current = current.next
   return True
+
+# test_01
+a = Node(7)
+b = Node(7)
+c = Node(7)
+
+a.next = b
+b.next = c
+
+# 7 -> 7 -> 7
+
+assert is_univalue_list(a) == True
+
+# test_02
+a = Node(7)
+b = Node(7)
+c = Node(4)
+
+a.next = b
+b.next = c
+
+# 7 -> 7 -> 4
+
+assert is_univalue_list(a) == False
+
+# test_03
+u = Node(2)
+v = Node(2)
+w = Node(2)
+x = Node(2)
+y = Node(2)
+
+u.next = v
+v.next = w
+w.next = x
+x.next = y
+
+# 2 -> 2 -> 2 -> 2 -> 2
+
+assert is_univalue_list(u) == True
+
+# test_04
+u = Node(2)
+v = Node(2)
+w = Node(3)
+x = Node(3)
+y = Node(2)
+
+u.next = v
+v.next = w
+w.next = x
+x.next = y
+
+# 2 -> 2 -> 3 -> 3 -> 2
+
+assert is_univalue_list(u) == False
+
+# test_05
+z = Node('z')
+
+# z
+
+assert is_univalue_list(z) == True
+
+# test_06
+u = Node(2)
+v = Node(1)
+w = Node(2)
+x = Node(2)
+y = Node(2)
+
+u.next = v
+v.next = w
+w.next = x
+x.next = y
+
+# 2 -> 1 -> 2 -> 2 -> 2
+
+assert is_univalue_list(u) == False
