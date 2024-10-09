7b5027e6f20dd8ec66dd381d5f879bb788921ff0
Connor <connordbrown@gmail.com>
2024-10-08 20:03:46 -0600

    add cashier algorithm

---
+++ cashier_alg.py
@@ -0,0 +1,19 @@
+v = [25, 10, 5, 1]
+n = 65
+r = len(v) - 1
+
+def change(n, v, r):
+    d = [0, 0, 0, 0]
+    k = 0    
+    while n > 0:
+        while k < r and v[k] > n:
+            k = k + 1
+        if v[k] > n:
+            return 'no solution'
+        else:
+            d[k] += 1
+            n -= v[k]
+    return d 
+            
+    
+print(change(n, v, r))
